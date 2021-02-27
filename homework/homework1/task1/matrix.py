9ecd1b77e81ffb80b6fe4ab6f7802c51a2d95d9b
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-02-27 10:27:30 +0300

    Added some annotations

--- homework/homework1/task1/matrix.py
+++ homework/homework1/task1/matrix.py
@@ -6,7 +6,7 @@ from homework.homework1.task1.vector import Vector
 class Matrix:
     __matrix: List[List[Union[int, float]]]
 
-    def __init__(self, *matrix: List[Union[int, float]]):
+    def __init__(self, *matrix: List[Union[int, float]]) -> None:
         if not matrix:
             raise ValueError("Matrix must contain at least one element.")
 
@@ -21,16 +21,16 @@ class Matrix:
 
         self.__matrix = list(matrix)
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         return isinstance(other, Matrix) and self.__matrix == other.__matrix
 
-    def __str__(self):
+    def __str__(self) -> str:
         output = ""
         for row in self.__matrix:
             output += " ".join(map(str, row)) + "\n"
         return output.strip("\n")
 
-    def __add__(self, other: "Matrix"):
+    def __add__(self, other: "Matrix") -> "Matrix":
         if not self.__are_dimensions_equal(other):
             raise ValueError("Matrices do not have the same dimensions.")
 
@@ -38,7 +38,7 @@ class Matrix:
             *[[x + y for x, y in zip(self.__matrix[i], other.__matrix[i])] for i in range(self.__number_of_rows())]
         )
 
-    def __mul__(self, other: "Matrix"):
+    def __mul__(self, other: "Matrix") -> "Matrix":
         if not self.__can_be_multiplied(other):
             raise ValueError("Matrices have incompatible dimensions.")
 
@@ -52,22 +52,22 @@ class Matrix:
             ]
         )
 
-    def transpose(self):
+    def transpose(self) -> "Matrix":
         return Matrix(
             *[[self.__matrix[i][j] for i in range(self.__number_of_rows())] for j in range(self.__number_of_columns())]
         )
 
-    def __number_of_columns(self):
+    def __number_of_columns(self) -> int:
         return len(self.__matrix[0])
 
-    def __number_of_rows(self):
+    def __number_of_rows(self) -> int:
         return len(self.__matrix)
 
-    def __are_dimensions_equal(self, other: "Matrix"):
+    def __are_dimensions_equal(self, other: "Matrix") -> bool:
         return (
             self.__number_of_rows() == other.__number_of_rows()
             and self.__number_of_columns() == other.__number_of_columns()
         )
 
-    def __can_be_multiplied(self, other: "Matrix"):
+    def __can_be_multiplied(self, other: "Matrix") -> bool:
         return self.__number_of_columns() == other.__number_of_rows()
