1c18897cb3ac1574345a214976e94c6d2062d842
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-02-27 10:37:06 +0300

    Added some annotations

--- homework/homework1/task1/vector.py
+++ homework/homework1/task1/vector.py
@@ -5,23 +5,23 @@ from typing import List, Union
 class Vector:
     __elements: List[Union[int, float]]
 
-    def __init__(self, *elements: Union[int, float]):
+    def __init__(self, *elements: Union[int, float]) -> None:
         if not elements:
             raise ValueError("The vector must contain at least one element.")
 
         self.__elements = list(elements)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"({', '.join(map(str, self.__elements))})"
 
-    def dot(self, other: "Vector"):
+    def dot(self, other: "Vector") -> Union[int, float]:
         if len(self.__elements) != len(other.__elements):
             raise ValueError("The dimensions of the vectors do not match.")
 
         return sum(x * y for x, y in zip(self.__elements, other.__elements))
 
-    def norm(self):
+    def norm(self) -> float:
         return sqrt(self.dot(self))
 
-    def angle(self, other: "Vector"):
+    def angle(self, other: "Vector") -> float:
         return degrees(acos(self.dot(other) / (self.norm() * other.norm())))
