5205145839b7cf052ed36deb1373de536b8345d2
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-03-02 15:45:52 +0300

    Added magic lines

--- homework/homework2/task2/cache.py
+++ homework/homework2/task2/cache.py
@@ -1,6 +1,19 @@
 import functools
 from collections import OrderedDict
-from typing import Hashable
+from typing import Hashable, TypeVar, Callable, cast, Protocol
+
+F = TypeVar("F", bound=Callable[..., object])
+
+
+class ActionWithAttributes(Protocol[F]):
+    cache: OrderedDict
+    __call__: F
+
+
+def action_with_attributes(action: F) -> ActionWithAttributes[F]:
+    action_with_attributes = cast(ActionWithAttributes[F], action)
+    action_with_attributes.cache = OrderedDict()
+    return action_with_attributes
 
 
 def cache_decorator(func=None, *, size: int = 0):
@@ -10,6 +23,7 @@ def cache_decorator(func=None, *, size: int = 0):
     if func is None:
         return lambda f: cache_decorator(f, size=size)
 
+    @action_with_attributes
     @functools.wraps(func)
     def inner(*args: Hashable, **kwargs: Hashable):
         if size == 0:
