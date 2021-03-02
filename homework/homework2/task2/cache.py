34f85adaa507aab5cd07ca97f53cf9cc8fd0650a
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-03-02 13:17:59 +0300

    Fixed issues

--- homework/homework2/task2/cache.py
+++ homework/homework2/task2/cache.py
@@ -1,39 +1,35 @@
 import functools
 from collections import OrderedDict
+from typing import Hashable
 
 
 def cache_decorator(func=None, *, size: int = 0):
-    if size <= 0:
-        raise ValueError("Size must be a positive number.")
+    if size < 0:
+        raise ValueError("Size must be a non-negative number.")
 
     if func is None:
         return lambda f: cache_decorator(f, size=size)
 
-    cache: OrderedDict = OrderedDict()
-
     @functools.wraps(func)
-    def inner(*args, **kwargs):
+    def inner(*args: Hashable, **kwargs: Hashable):
         if size == 0:
             return func(*args, **kwargs)
 
         key = make_key(*args, **kwargs)
-        if key in cache:
-            return cache[key]
+        if key in inner.cache:
+            return inner.cache[key]
 
         value = func(*args, **kwargs)
 
-        if len(cache) >= size:
-            cache.popitem(last=False)
-        cache[key] = value
+        if len(inner.cache) >= size:
+            inner.cache.popitem(last=False)
+        inner.cache[key] = value
 
         return value
 
-    def make_key(*args, **kwargs) -> tuple:
-        key = args
+    inner.cache = OrderedDict()
 
-        for item in kwargs.items():
-            key += item
-
-        return key
+    def make_key(*args: Hashable, **kwargs: Hashable) -> tuple:
+        return args + tuple(sorted(kwargs.items()))
 
     return inner
