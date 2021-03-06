4610a2648e83033565d1c661146060f56422944b
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-03-06 17:54:18 +0300

    Added tests

---
+++ tests/homework2/task3/test_smart_args.py
@@ -0,0 +1,84 @@
+import datetime
+from time import sleep
+from unittest import TestCase
+
+from homework.homework2.task3.smart_args import smart_args, Evaluated, Isolated
+
+
+class SmartArgsTestCase(TestCase):
+    def test_isolated_inside_evaluated(self):
+        with self.assertRaises(ValueError) as context:
+
+            @smart_args
+            def check(*, arg=Evaluated(Isolated())):
+                pass
+
+        self.assertTrue("Isolated cannot be used with Evaluated" in str(context.exception))
+
+    def test_evaluated_inside_isolated(self):
+        with self.assertRaises(ValueError) as context:
+
+            @smart_args
+            def check(*, arg=Isolated(Evaluated(lambda: 42))):
+                pass
+
+        self.assertTrue("Evaluated cannot be used with Isolated" in str(context.exception))
+
+    def test_function_with_params_as_argument_of_evaluated(self):
+        with self.assertRaises(ValueError) as context:
+
+            @smart_args
+            def check(*, arg=Evaluated(lambda x, y, z: 42)):
+                pass
+
+        self.assertTrue("Functions with arguments are not supported by Evaluated" in str(context.exception))
+
+    def test_isolated_keyword_argument(self):
+        @smart_args
+        def list_sort(*, input_list=Isolated()):
+            input_list.sort()
+
+        input_list = [-644, 883, 796, -608, -985]
+
+        list_sort(input_list=input_list)
+
+        self.assertEqual(input_list, [-644, 883, 796, -608, -985])
+
+    def test_isolated_positional_argument(self):
+        @smart_args(positional_arguments_included=True)
+        def list_sort(input_list, /):
+            input_list.sort()
+
+        input_list = [839, 739, -923, -293, 229]
+
+        list_sort(Isolated(input_list))
+
+        self.assertEqual(input_list, [839, 739, -923, -293, 229])
+
+    def test_evaluated_keyword_argument(self):
+        def get_timestamp():
+            return datetime.datetime.now().timestamp()
+
+        @smart_args
+        def check(*, x=get_timestamp(), y=Evaluated(get_timestamp)):
+            return x, y
+
+        x1, y1 = check()
+        sleep(0.001)
+        x2, y2 = check()
+
+        self.assertTrue(x1 == x2 and y1 < y2)
+
+    def test_evaluated_positional_argument(self):
+        def get_timestamp():
+            return datetime.datetime.now().timestamp()
+
+        @smart_args(positional_arguments_included=True)
+        def check(x, /):
+            return x
+
+        x1 = check(Evaluated(get_timestamp))
+        sleep(0.001)
+        x2 = check(Evaluated(get_timestamp))
+
+        self.assertTrue(x1 < x2)
