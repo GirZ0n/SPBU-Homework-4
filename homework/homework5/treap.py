26e9a3213095d3c2f247f448eebf2447ccac9470
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-04-05 17:57:14 +0300

    Small fixes

--- homework/homework5/treap.py
+++ homework/homework5/treap.py
@@ -1,12 +1,13 @@
 from abc import abstractmethod, ABCMeta
 from collections.abc import MutableMapping
+from json import JSONDecoder, JSONEncoder, dumps
 from typing import Optional, TypeVar, Generic, Iterator, Any
 
 from node import Node
 
 
 class Treap(MutableMapping):
-    size: int = 0
+    __size: int = 0
     __head: Optional[Node] = None
 
     def __str__(self) -> str:
@@ -22,31 +23,44 @@ class Treap(MutableMapping):
             else:
                 self.__head = self.__head.insert(key, value)
 
-            self.size += 1
+            self.__size += 1
+
+    def insert(self, key, value, priority) -> None:
+        if key in self:
+            raise KeyError(f"The given key ({key}) is already in the tree")
+
+        if self.__head is None:
+            self.__head = Node(key, value, priority)
+        else:
+            self.__head = self.__head.insert(key, value, priority)
+
+        self.__size += 1
 
     def __contains__(self, key) -> bool:
         return self.__head is not None and key in self.__head
 
     def __delitem__(self, key) -> None:
-        if key not in self:
-            raise KeyError(f"There is no such key ({key}) in the treap")
-
         if self.__head is None:
             raise KeyError("The treap is empty")
 
-        self.__head = self.__head.remove(key)
-
-        self.size -= 1
-
-    def __getitem__(self, key) -> Any:
         if key not in self:
             raise KeyError(f"There is no such key ({key}) in the treap")
 
-        assert self.__head is not None
+        self.__head = self.__head.remove(key)
+
+        self.__size -= 1
+
+    def __getitem__(self, key) -> Any:
+        if self.__head is None:
+            raise KeyError("The treap is empty")
+
+        if key not in self:
+            raise KeyError(f"There is no such key ({key}) in the treap")
+
         return self.__head.get(key)
 
     def __len__(self) -> int:
-        return self.size
+        return self.__size
 
     def __iter__(self) -> Iterator[Any]:
         if self.__head is None:
