86c685c2d4076aff1fd9dd174371baa97ff3c352
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-04-05 17:57:23 +0300

    Added tests

---
+++ tests/homework5/test_node.py
@@ -0,0 +1,84 @@
+import json
+import unittest
+from pathlib import Path
+
+from homework.homework5.node import Node
+
+resources = Path("./resources")
+
+
+def create_full_node() -> Node:
+    node = Node(1, 1, 1)
+    node = node.insert(2, 2, 4)
+    node = node.insert(3, 3, 3)
+    node = node.insert(4, 4, 7)
+    node = node.insert(5, 5, 5)
+    node = node.insert(6, 6, 6)
+    node = node.insert(7, 7, 2)
+    return node
+
+
+class NodeTestCase(unittest.TestCase):
+    def test_insert_without_child(self):
+        actual = Node(1, 1, 1)
+
+        with open(resources / "without_child.json") as f:
+            expected = json.loads(f.read(), cls=Node.NodeDecoder)
+
+        self.assertEqual(expected, actual)
+
+    def test_insert_only_left_child(self):
+        actual = Node(1, 1, 1)
+        actual = actual.insert(2, 2, 4)
+
+        with open(resources / "only_left_child.json") as f:
+            expected = json.loads(f.read(), cls=Node.NodeDecoder)
+
+        self.assertEqual(expected, actual)
+
+    def test_insert_only_right_child(self):
+        actual = Node(3, 3, 1)
+        actual = actual.insert(2, 2, 4)
+
+        with open(resources / "only_right_child.json") as f:
+            expected = json.loads(f.read(), cls=Node.NodeDecoder)
+
+        self.assertEqual(expected, actual)
+
+    def test_insert_full_treap(self):
+        actual = create_full_node()
+
+        with open(resources / "full_treap.json") as f:
+            expected = json.loads(f.read(), cls=Node.NodeDecoder)
+
+        self.assertEqual(expected, actual)
+
+    def test_contains_key_in_node(self):
+        node = create_full_node()
+        self.assertTrue(5 in node)
+
+    def test_contains_key_not_in_node(self):
+        node = create_full_node()
+        self.assertFalse(42 in node)
+
+    def test_iterator(self):
+        node = create_full_node()
+        self.assertEqual(list(node), [4, 2, 1, 3, 6, 5, 7])
+
+    def test_reversed(self):
+        node = create_full_node()
+        self.assertEqual(list(reversed(node)), [1, 3, 2, 5, 7, 6, 4])
+
+    def test_get(self):
+        node = create_full_node()
+        self.assertEqual(node.get(3), 3)
+
+    def test_update(self):
+        node = create_full_node()
+        node.update(3, 42)
+        self.assertEqual(node.get(3), 42)
+
+    def test_remove(self):
+        node = create_full_node()
+        node = node.remove(5)
+        self.assertFalse(5 in node)
---
+++ tests/homework5/test_treap.py
@@ -0,0 +1,111 @@
+import json
+import unittest
+from pathlib import Path
+
+from homework.homework5.treap import Treap
+from tests.tests_utils import check_message
+
+resources = Path("./resources")
+
+
+def create_full_treap() -> Treap:
+    treap = Treap()
+    treap.insert(1, 1, 1)
+    treap.insert(2, 2, 4)
+    treap.insert(3, 3, 3)
+    treap.insert(4, 4, 7)
+    treap.insert(5, 5, 5)
+    treap.insert(6, 6, 6)
+    treap.insert(7, 7, 2)
+    return treap
+
+
+class TreapTestCase(unittest.TestCase):
+    def test_contains_key_in_treap(self):
+        treap = create_full_treap()
+        self.assertTrue(3 in treap)
+
+    def test_contains_key_not_in_treap(self):
+        treap = create_full_treap()
+        self.assertFalse(42 in treap)
+
+    def test_delete_empty_treap(self):
+        treap = Treap()
+        with self.assertRaises(KeyError) as context:
+            del treap[42]
+
+        self.assertTrue(check_message(context, "The treap is empty"))
+
+    def test_delete_key_not_in_treap(self):
+        treap = create_full_treap()
+        with self.assertRaises(KeyError) as context:
+            del treap[42]
+
+        self.assertTrue(check_message(context, "There is no such key (42) in the treap"))
+
+    def test_delete_key_in_treap(self):
+        treap = create_full_treap()
+        del treap[3]
+
+        self.assertTrue(3 not in treap)
+
+    def test_get_empty_treap(self):
+        treap = Treap()
+        with self.assertRaises(KeyError) as context:
+            treap[42]
+
+        self.assertTrue(check_message(context, "The treap is empty"))
+
+    def test_get_key_not_in_treap(self):
+        treap = create_full_treap()
+        with self.assertRaises(KeyError) as context:
+            treap[42]
+
+        self.assertTrue(check_message(context, "There is no such key (42) in the treap"))
+
+    def test_get_key_in_treap(self):
+        treap = create_full_treap()
+        result = treap[3]
+
+        self.assertEqual(result, 3)
+
+    def test_len_empty_treap(self):
+        treap = Treap()
+        self.assertEqual(len(treap), 0)
+
+    def test_len_non_empty_treap(self):
+        treap = Treap()
+        for i in range(10):
+            treap[i] = i
+        self.assertEqual(len(treap), 10)
+
+    def test_iterator_empty_treap(self):
+        treap = Treap()
+        self.assertEqual(list(treap), [])
+
+    def test_iterator_non_empty_treap(self):
+        treap = create_full_treap()
+        self.assertEqual(list(treap), [4, 2, 1, 3, 6, 5, 7])
+
+    def test_reversed_empty_treap(self):
+        treap = Treap()
+        self.assertEqual(list(reversed(treap)), [])
+
+    def test_reversed_non_empty_treap(self):
+        treap = create_full_treap()
+        self.assertEqual(list(reversed(treap)), [1, 3, 2, 5, 7, 6, 4])
+
+    def test_set_empty_treap(self):
+        treap = Treap()
+        treap[5] = 5
+        self.assertTrue(5 in treap)
+
+    def test_set_key_not_in_treap(self):
+        treap = create_full_treap()
+        treap[42] = 42
+        self.assertTrue(treap[42] == 42)
+
+    def test_set_key_in_treap(self):
+        treap = create_full_treap()
+        treap[3] = 42
+        self.assertTrue(treap[3], 42)
