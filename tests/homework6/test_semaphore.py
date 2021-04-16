9f4ed6ebff8d66ba5ed6aadcf82fcdf57f672470
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-04-16 17:56:32 +0300

    Improved tests

--- tests/homework6/test_semaphore.py
+++ tests/homework6/test_semaphore.py
@@ -1,9 +1,28 @@
 import unittest
+from typing import Callable
 from threading import Thread
 
 from tests.tests_utils import check_message
 
 from homework.homework6.semaphore import Semaphore
+
+
+def run_threads(number_of_threads: int, function: Callable):
+    threads = [Thread(target=function) for _ in range(number_of_threads)]
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+
+class SemaphoreRaisesTestCase(unittest.TestCase):
+    def test_incorrect_semaphore_size(self):
+        with self.assertRaises(ValueError) as context:
+            Semaphore(-206)
+
+        self.assertTrue(check_message(context, "Semaphore initial size must be >= 0"))
 
 
 class SemaphoreIncrementTestCase(unittest.TestCase):
@@ -21,46 +40,65 @@ class SemaphoreIncrementTestCase(unittest.TestCase):
             for i in range(100_000):
                 self.count += 1
 
-    def test_incorrect_semaphore_size(self):
-        with self.assertRaises(ValueError) as context:
-            Semaphore(-206)
-
-        self.assertTrue(check_message(context, "Semaphore initial size must be >= 0"))
-
     def test_increment_one_thread(self):
-        tread = Thread(target=self.increment())
-
-        tread.start()
-        tread.join()
+        run_threads(1, self.increment)
 
         self.assertEqual(self.count, 100_000)
 
     def test_increment_ten_threads(self):
-        threads = [Thread(target=self.increment()) for _ in range(10)]
-
-        for thread in threads:
-            thread.start()
-
-        for thread in threads:
-            thread.join()
+        run_threads(10, self.increment)
 
         self.assertEqual(self.count, 100_000 * 10)
 
     def test_increment_with_long_critical_section_one_thread(self):
-        tread = Thread(target=self.increment_with_long_critical_section())
-
-        tread.start()
-        tread.join()
+        run_threads(1, self.increment_with_long_critical_section)
 
         self.assertEqual(self.count, 100_000)
 
     def test_increment_with_long_critical_section_ten_threads(self):
-        threads = [Thread(target=self.increment_with_long_critical_section()) for _ in range(10)]
-
-        for thread in threads:
-            thread.start()
-
-        for thread in threads:
-            thread.join()
+        run_threads(10, self.increment_with_long_critical_section)
 
         self.assertEqual(self.count, 100_000 * 10)
+
+
+class SemaphoreSquareTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.semaphore = Semaphore()
+        self.numbers = []
+        self.result = []
+
+    def square(self):
+        while True:
+            with self.semaphore:
+                if self.numbers:
+                    self.result.append(self.numbers.pop(0) ** 2)
+                else:
+                    return
+
+    def test_1_number_one_thread(self):
+        self.numbers.append(104)
+
+        run_threads(1, self.square)
+
+        self.assertEqual(self.result, [104 ** 2])
+
+    def test_1_number_10_threads(self):
+        self.numbers.append(172)
+
+        run_threads(10, self.square)
+
+        self.assertEqual(self.result, [172 ** 2])
+
+    def test_many_numbers_1_thread(self):
+        self.numbers.extend(range(100_000))
+
+        run_threads(1, self.square)
+
+        self.assertEqual(self.result, list(map(lambda x: x ** 2, range(100_000))))
+
+    def test_many_numbers_10_thread(self):
+        self.numbers.extend(range(100_000))
+
+        run_threads(10, self.square)
+
+        self.assertEqual(self.result, list(map(lambda x: x ** 2, range(100_000))))
