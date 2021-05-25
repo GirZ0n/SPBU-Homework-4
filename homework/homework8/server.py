8348ea33c3007a1ac626e7e17e45701ba7a4ccfc
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-05-25 18:20:06 +0300

    Fixed small issue

--- homework/homework8/server.py
+++ homework/homework8/server.py
@@ -8,16 +8,16 @@ from homework.homework8.object_detector import ObjectDetector
 
 app = Flask(__name__)
 
+classes_path = Path("./resources/coco.names")
+configuration_path = Path("./resources/yolov3.cfg")
+weights_path = Path("./resources/yolov3.weights")
+detector = ObjectDetector(classes_path, configuration_path, weights_path)
+
 
 @app.route("/", methods=["POST"])
 def test():
     array = np.frombuffer(request.data, np.uint8)
     image = cv2.imdecode(array, cv2.IMREAD_COLOR)
-
-    classes_path = Path("./resources/coco.names")
-    configuration_path = Path("./resources/yolov3.cfg")
-    weights_path = Path("./resources/yolov3.weights")
-    detector = ObjectDetector(classes_path, configuration_path, weights_path)
 
     modified_image = detector.detect(image)
 
